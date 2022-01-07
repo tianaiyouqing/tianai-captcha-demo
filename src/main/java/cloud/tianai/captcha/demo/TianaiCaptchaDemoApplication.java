09b96813f1c59346204d95e4a057211283179991
天爱有情 <tianaiyouqing@163.com>
2022-01-07 10:01:58 +0800

    加点日志

--- src/main/java/cloud/tianai/captcha/demo/TianaiCaptchaDemoApplication.java
+++ src/main/java/cloud/tianai/captcha/demo/TianaiCaptchaDemoApplication.java
@@ -3,16 +3,20 @@ package cloud.tianai.captcha.demo;
 import cloud.tianai.captcha.slider.SliderCaptchaApplication;
 import cloud.tianai.captcha.vo.CaptchaResponse;
 import cloud.tianai.captcha.vo.SliderCaptchaVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 
+@Slf4j
 @Controller
 @SpringBootApplication
 public class TianaiCaptchaDemoApplication {
@@ -29,19 +33,28 @@ public class TianaiCaptchaDemoApplication {
 
     @GetMapping("/gen")
     @ResponseBody
-    public CaptchaResponse<SliderCaptchaVO> genCaptcha() {
+    public CaptchaResponse<SliderCaptchaVO> genCaptcha(HttpServletRequest request) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         CaptchaResponse<SliderCaptchaVO> response = sliderCaptchaApplication.generateSliderCaptcha();
+        stopWatch.stop();
+        // 打点日志记录
+        log.info("[生成验证码], IP={}, id={}, 耗时={}ms", IPUtils.getIpAddr(request), response.getId(), stopWatch.getTotalTimeMillis());
         return response;
     }
 
-
     @GetMapping("/check")
     @ResponseBody
-    public boolean checkCaptcha(@RequestParam("id") String id, @RequestParam("percentage") Float percentage) {
+    public boolean checkCaptcha(@RequestParam("id") String id,
+                                @RequestParam("percentage") Float percentage,
+                                HttpServletRequest request) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         boolean matching = sliderCaptchaApplication.matching(id, percentage);
+        stopWatch.stop();
+        log.info("[校验验证码], IP={}, id={},percentage={},耗时={}ms", IPUtils.getIpAddr(request), id, percentage, stopWatch.getTotalTimeMillis());
         return matching;
     }
-
 
 
     public static void main(String[] args) {
