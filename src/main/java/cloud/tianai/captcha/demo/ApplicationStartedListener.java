6b7bc631c2fd0b9bc8ab083411e4c8cae7131ea9
天爱有情 <tianaiyouqing@163.com>
2022-02-09 10:58:31 +0800

    +个新模板

--- src/main/java/cloud/tianai/captcha/demo/ApplicationStartedListener.java
+++ src/main/java/cloud/tianai/captcha/demo/ApplicationStartedListener.java
@@ -3,11 +3,18 @@ package cloud.tianai.captcha.demo;
 import cloud.tianai.captcha.slider.SliderCaptchaApplication;
 import cloud.tianai.captcha.template.slider.Resource;
 import cloud.tianai.captcha.template.slider.ResourceStore;
+import cloud.tianai.captcha.template.slider.SliderCaptchaConstant;
 import cloud.tianai.captcha.template.slider.SliderCaptchaResourceManager;
+import cloud.tianai.captcha.template.slider.provider.ClassPathResourceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static cloud.tianai.captcha.template.slider.DefaultSliderCaptchaTemplate.DEFAULT_SLIDER_IMAGE_TEMPLATE_PATH;
 
 @Component
 public class ApplicationStartedListener implements ApplicationListener<ApplicationStartedEvent> {
@@ -21,6 +28,17 @@ public class ApplicationStartedListener implements ApplicationListener<Applicati
         ResourceStore resourceStore = sliderCaptchaResourceManager.getResourceStore();
         // 清除内置的背景图片
         resourceStore.clearResources();
+//        resourceStore.clearTemplates();
+        Map<String, Resource> template2 = new HashMap<>(4);
+        template2.put(SliderCaptchaConstant.TEMPLATE_ACTIVE_IMAGE_NAME, new Resource(ClassPathResourceProvider.NAME, "captcha-template/bb.png"));
+        template2.put(SliderCaptchaConstant.TEMPLATE_FIXED_IMAGE_NAME, new Resource(ClassPathResourceProvider.NAME, "captcha-template/aa.png"));
+        template2.put(SliderCaptchaConstant.TEMPLATE_MATRIX_IMAGE_NAME, new Resource(ClassPathResourceProvider.NAME, DEFAULT_SLIDER_IMAGE_TEMPLATE_PATH.concat("/2/matrix.png")));
+        // 让新模板出现的概率变大
+        resourceStore.addTemplate(template2);
+        resourceStore.addTemplate(template2);
+        resourceStore.addTemplate(template2);
+        resourceStore.addTemplate(template2);
+        resourceStore.addTemplate(template2);
 
         // 添加自定义背景图片
         resourceStore.addResource(new Resource("classpath", "bgimages/a.jpg"));
