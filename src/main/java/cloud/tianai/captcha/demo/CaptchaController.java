2ae2270acdec1c3f202642d82fa27e280838f0b6
天爱有情 <tianaiyouqing@163.com>
2023-09-15 09:54:38 +0800

    接入新的UI

--- src/main/java/cloud/tianai/captcha/demo/CaptchaController.java
+++ src/main/java/cloud/tianai/captcha/demo/CaptchaController.java
@@ -2,16 +2,19 @@ package cloud.tianai.captcha.demo;
 
 
 import cloud.tianai.captcha.common.constant.CaptchaTypeConstant;
+import cloud.tianai.captcha.common.response.ApiResponse;
 import cloud.tianai.captcha.spring.application.ImageCaptchaApplication;
 import cloud.tianai.captcha.spring.plugins.secondary.SecondaryVerificationApplication;
 import cloud.tianai.captcha.spring.vo.CaptchaResponse;
 import cloud.tianai.captcha.spring.vo.ImageCaptchaVO;
 import cloud.tianai.captcha.validator.common.model.dto.ImageCaptchaTrack;
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class CaptchaController {
@@ -20,11 +23,24 @@ public class CaptchaController {
     @Autowired
     private ImageCaptchaApplication imageCaptchaApplication;
 
-    @GetMapping("/gen")
+    @RequestMapping("/gen")
     @ResponseBody
     public CaptchaResponse<ImageCaptchaVO> genCaptcha(HttpServletRequest request, @RequestParam(value = "type", required = false)String type) {
         if (StringUtils.isBlank(type)) {
             type = CaptchaTypeConstant.SLIDER;
+        }
+        if ("RANDOM".equals(type)) {
+            int i = ThreadLocalRandom.current().nextInt(0, 4);
+            if (i == 0) {
+                type = CaptchaTypeConstant.SLIDER;
+            } else if (i == 1) {
+                type = CaptchaTypeConstant.CONCAT;
+            } else if (i == 2) {
+                type = CaptchaTypeConstant.ROTATE;
+            } else{
+                type = CaptchaTypeConstant.WORD_IMAGE_CLICK;
+            }
+
         }
         CaptchaResponse<ImageCaptchaVO> response = imageCaptchaApplication.generateCaptcha(type);
         return response;
@@ -32,10 +48,15 @@ public class CaptchaController {
 
     @PostMapping("/check")
     @ResponseBody
-    public boolean checkCaptcha(@RequestParam("id") String id,
-                                @RequestBody ImageCaptchaTrack imageCaptchaTrack,
-                                HttpServletRequest request) {
-        return imageCaptchaApplication.matching(id, imageCaptchaTrack).isSuccess();
+    public ApiResponse<?> checkCaptcha(@RequestBody Data data,
+                                    HttpServletRequest request) {
+        return imageCaptchaApplication.matching(data.getId(), data.getData());
+    }
+
+    @lombok.Data
+    public static class Data {
+        private String  id;
+        private ImageCaptchaTrack data;
     }
 
     /**
